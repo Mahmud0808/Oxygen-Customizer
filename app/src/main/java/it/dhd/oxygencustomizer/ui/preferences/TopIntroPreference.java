885ef14274f83ab769e288b61fad370e81eae52e
DHD2280 <luigiconte98@gmail.com>
2024-11-17 16:14:47 +0100

    Minus UI Changes
    Signed-off-by: DHD2280 <luigiconte98@gmail.com>

--- app/src/main/java/it/dhd/oxygencustomizer/ui/preferences/TopIntroPreference.java
+++ app/src/main/java/it/dhd/oxygencustomizer/ui/preferences/TopIntroPreference.java
@@ -14,19 +14,23 @@ public class TopIntroPreference extends Preference {
     public TopIntroPreference(Context context) {
         super(context);
         setLayoutResource(R.layout.top_intro_preference);
+        if (getIcon() == null) {
+            setIcon(R.drawable.settingslib_ic_info_outline_24);
+        }
         setSelectable(false);
     }
 
     public TopIntroPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         setLayoutResource(R.layout.top_intro_preference);
+        if (getIcon() == null) {
+            setIcon(R.drawable.settingslib_ic_info_outline_24);
+        }
         setSelectable(false);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
-        holder.setDividerAllowedAbove(false);
-        holder.setDividerAllowedBelow(false);
     }
 }
